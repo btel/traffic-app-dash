4e6ab76b32ad3fc18199591eec816055539611a8
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2019-04-02 23:55:58 +0200

    add dropdown menu

--- src/App.js
+++ src/App.js
@@ -10,32 +10,51 @@ class TrafficSeries extends Component {
         this.state = {
             error: null,
             isLoaded: false,
-            data: []}
+            data: [],
+            period: 240}
+        this.handlePeriodChange = this.handlePeriodChange.bind(this);
     }
 
-    
-    componentDidMount() {
-        fetch(process.env.REACT_APP_BACKEND_URL + "/api/traffic?period=240")
+    handlePeriodChange(e) {
+        this.fetchData(e.target.value);
+    }
+
+    fetchData(period) {
+        fetch(process.env.REACT_APP_BACKEND_URL + "/api/traffic?period=" + period)
             .then(res => res.json())
             .then(
                   (result) => {
                       this.setState({
                           isLoaded: true,
-                          data: result.data
+                          data: result.data,
+                          period : period
                       });
                   },
                   (error) => {
                       this.setState({
                           isLoaded: true,
+                          period: period,
                           error: error});
              }
             )
+
+    }
+
+    
+    componentDidMount() {
+        this.fetchData(this.state.period);
     }
 
     render() { return (
-              <LineChart width={400} height={400} data={this.state.data} className='chart'>
-                <Line type="monotone" dataKey="debit" stroke="#8884d8" />
-              </LineChart>
+                <div>
+                <LineChart width={400} height={400} data={this.state.data} className='chart'>
+                  <Line type="monotone" dataKey="debit" stroke="#8884d8" />
+                </LineChart>
+                <select id="period" name="period" value={this.state.period} onChange={this.handlePeriodChange}>
+                  <option value="24">1 day</option>
+                  <option value="240">10 days</option>
+                </select>
+                </div>
     );
   }
 }
